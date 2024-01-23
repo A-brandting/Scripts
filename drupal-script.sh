89756aff745cdfbc8bca87e32732b5ece1e9c376
A-brandting <156856324+A-brandting@users.noreply.github.com>
2024-01-23 22:10:38 +0100

    Create drupal-script.sh

---
+++ drupal-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Rename no-debian-php
+mv /etc/apt/preferences.d/no-debian-php /etc/apt/preferences.d/no-debian-php.backup
+
+# Install Drupal Core
+cd /opt/drupal
+composer install && composer require drupal/ldap_auth
+apt update -y
+apt install php8.2-ldap -y
+
+# Create missing files/folders and gives write permissions
+mkdir /opt/drupal/web/sites/default/files
+chmod a+w /opt/drupal/web/sites/default/files
+cp /opt/drupal/web/sites/default/default.settings.php /opt/drupal/web/sites/default/settings.php
+chmod a+w /opt/drupal/web/sites/default/settings.php
+
+# Creates and modifies php.ini
+cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
+sed -i '927s/;//' /usr/local/etc/php/php.ini
+sed -i '928i\extension=php_ldap.so' /usr/local/etc/php/php.ini
+
+#Install dependencies 
+apt update
+apt install -y libldap2-dev
+docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
+docker-php-ext-install ldap
+service apache2 restart
+exit
+
